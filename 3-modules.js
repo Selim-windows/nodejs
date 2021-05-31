2e5c10c72004aad0a812fec7240dc715db197ac0
selim-windows <selim-windows@gmx.de>
2021-05-31 22:00:36 +0200

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+const names = require('./4-names.js');
+const sayHi = require('./5-utils.js');
+const data = require('./6-alternativ-flavor.js');
+require('./7-mind-grenade.js');
+// sayHi("susan");
+// sayHi(names.john);
+// sayHi(names.peter);
